2c3607b140d8aafb32a4a47bf06c46c805d459ac
anilyadav-09 <anilknby97@gmail.com>
2025-09-04 18:33:51 +0530

    sampark-fllow-up

---
+++ src/test/java/com/sampark/sampark_followup/SamparkFollowupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sampark.sampark_followup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamparkFollowupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
